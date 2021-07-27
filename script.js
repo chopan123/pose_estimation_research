8199e1dc4b6711fc6a4cf5a4a034d9b2f43b7f9c
chopan123 <chopancatrileo@gmail.com>
2021-07-27 18:50:21 -0400

    tutorial codelabs

---
+++ script.js
@@ -0,0 +1,66 @@
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+    return !!(navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia);
+  }
+  
+  // If webcam supported, add event listener to button for when user
+  // wants to activate it to call enableCam function which we will 
+  // define in the next step.
+  if (getUserMediaSupported()) {
+    enableWebcamButton.addEventListener('click', enableCam);
+  } else {
+    console.warn('getUserMedia() is not supported by your browser');
+  }
+  
+  // Placeholder function for next step. Paste over this in the next step.
+  function enableCam(event) {
+  }
+
+// Enable the live webcam view and start classification.
+function enableCam(event) {
+    // Only continue if the COCO-SSD has finished loading.
+    if (!model) {
+      return;
+    }
+    
+    // Hide the button once clicked.
+    event.target.classList.add('removed');  
+    
+    // getUsermedia parameters to force video but not audio.
+    const constraints = {
+      video: true
+    };
+  
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      video.srcObject = stream;
+      video.addEventListener('loadeddata', predictWebcam);
+    });
+  }
+  // Placeholder function for next step.
+function predictWebcam() {
+}
+
+// Pretend model has loaded so we can try out the webcam code.
+var model = true;
+demosSection.classList.remove('invisible');
+
+// Store the resulting model in the global scope of our app.
+var model = undefined;
+
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment 
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove('invisible');
+});
